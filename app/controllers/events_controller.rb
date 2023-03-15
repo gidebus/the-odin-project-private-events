3f9e9602ff3e5933278259e85cd307e381a32bdd
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-03-14 22:39:24 -0400

    ADD: Events controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,7 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
@@ -11,14 +12,40 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      flash[:success] = "Your event was saved."
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      flash[:success] = "#{@event.name} was edited."
+      redirect_to @event
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    flash[:success] = "#{@event.name} was deleted."
+    redirect_to root_path
+  end
+
+  private
+  
+  def event_params
+    params.require(:event).permit(:name, :date, :description, :location)
   end
 end
