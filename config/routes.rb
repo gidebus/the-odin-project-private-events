0a714de1ed98d5b8777c53b1e8f25bc32a90ccf6
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-03-15 12:45:52 -0400

    ADD: custom controllers to rotuer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :events
   root "events#index"
   # Defines the root path route ("/")
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  # devise_for :users
 end
