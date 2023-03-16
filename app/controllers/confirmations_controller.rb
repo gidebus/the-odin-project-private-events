acd0ba7e944ef7153bfbfab5191d9e63e25b3917
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-03-16 19:28:42 -0400

    ADD: working RSVP buttons and controller

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,12 +1,20 @@
 class ConfirmationsController < ApplicationController
-  def create
-    event = Event.find(params[:event_id])
-    @invitation = current_user.invitations.build(invitation_params)
-  end
 
-  def update
+  def create
+    event = Event.find(params[:attended_event_id])
+    user = User.find(params[:event_attendee_id])
+    @confirmation = Confirmation.new(event_attendee_id: user.id, attended_event_id: event.id)
+    redirect_to root_path, notice: "You are going to this event!" if @confirmation.save
   end
 
   def destroy
+    @confirmation = Confirmation.find(params[:id])
+    @confirmation.destroy
+    flash[:success] = "You were unregistered to this event."
+    redirect_to root_path
+  end
+
+  def confirmation_params
+    params.require(:confirmation).permit(:event_attendee_id, :attended_event_id)
   end
 end
