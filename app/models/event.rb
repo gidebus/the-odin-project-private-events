2850f2656dfd65f4142bd1492c15ce515f1ad073
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-03-17 15:34:07 -0400

    ADD: past and upcoming scopes to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   has_many :confirmations, foreign_key: :attended_event_id
   has_many :attendees, through: :confirmations, source: :event_attendee
   belongs_to :creator, class_name: 'User'
+
+  scope :upcoming, -> { where("date >= ? ", Date.today) }
+  scope :past, -> { where("date < ? ", Date.today) }
 end
